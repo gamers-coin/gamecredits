475b1c548aef3a5cc4a6ae83adec7481267a8334
gamers-coin <asksecinfo@gmail.com>
2014-03-19 00:43:27 +0100

    Fix

--- src/main.cpp
+++ src/main.cpp
@@ -1251,7 +1251,7 @@ unsigned int static GetNextWorkRequired(const CBlockIndex* pindexLast, const CBl
                 if (pindexLast->nHeight+1 >= 50) { DiffMode = 2; }
         }
         else {
-                if (pindexLast->nHeight+1 >= 15245) { DiffMode = 2; }
+                if (pindexLast->nHeight+1 >= 15260) { DiffMode = 2; }
         }
         
         if (DiffMode == 1) { return GetNextWorkRequired_V1(pindexLast, pblock); }
