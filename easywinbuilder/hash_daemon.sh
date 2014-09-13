fa060006e46ad1d08b31402f4fd1d4e8316c4198
gamers-coin <asksecinfo@gmail.com>
2014-09-13 22:26:45 +0200

    Remove Easywinbuilder
    Please use gitian for Windows Version.

--- easywinbuilder/hash_daemon.sh
+++
@@ -1,3 +0,0 @@
-echo Hash of daemon binary [experimental]...
-bash dahash.sh ../src/${COINNAME}d.exe
-echo phelix got: 
--- easywinbuilder/hash_qt.sh
+++
@@ -1,6 +0,0 @@
-echo Hash of Qt binary [experimental]...
-bash dahash.sh ../release/${COINNAME}-qt.exe
-echo phelix got: 7e1f2a6f435cedca4304640d58ca663212b8a2ca4267e6a5ca865ce28d90dc6a
-echo
-echo
-
